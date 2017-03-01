966b764d12f336128f2cda0d7cc168858502e080
nicola-onofri <nicola.onofri@gmail.com>
2017-03-02 00:56:34 +0100

    v0.4 empty template

---
+++ app/src/main/java/com/sandroide/nicola/sandroideproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sandroide.nicola.sandroideproject;
+
+import android.os.Bundle;
+
+import it.unibs.sandroide.lib.activities.SandroideBaseActivity;
+
+public class MainActivity extends SandroideBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
